86debc81bb8039f88ae6f1c79b28bb84ff120a54
Abdullah Al Zuhair <zuhair2025@gmail.com>
2018-12-29 01:56:35 +0600

    updated array.php

--- array.php
+++ array.php
@@ -1,9 +1,9 @@
 <?php
 $stdin = fopen('php://stdin', 'r');
 $stdout = fopen('php://stdout', 'w');
-// //$line = trim(fgets(STDIN)); // reads one line from STDIN
+
 fscanf(STDIN, "%s\n%s\n%s\n%s\n",$firstname,$lastname,$email,$address); // reads number from STDIN
-// fprintf(STDOUT,"N =%d E=%d",$node,$edge);
+
 
 $anika = ["firstname"=>"$firstname",
           "lastname"=>"$lastname",
@@ -13,7 +13,7 @@ $anika = ["firstname"=>"$firstname",
 foreach($anika as $key=>$value){
 
     echo "$key : $value\n";
-    // fprintf(STDOUT,"$key : $value\n");
+
 }
 
 

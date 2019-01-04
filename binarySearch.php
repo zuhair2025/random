cbd57864a0c31a5b55762f33326a004ba6e1de79
Abdullah Al Zuhair <zuhair2025@gmail.com>
2019-01-04 16:36:42 +0600

    modified

--- binarySearch.php
+++ binarySearch.php
@@ -5,13 +5,11 @@ function abc($a){
 $arr=[-5,-3,1,2,2,4,5,9,10];
 
 for($i=0;$i<count($arr);$i++){
-    while($arr[$i]==$a){
+    if($arr[$i]==$a){
         
-        echo "position is $i";
-        $i++;
-        
+        echo "value is $a position is $i";
+      
     }
 }
-
 }
-abc(-5);
+abc(9);
